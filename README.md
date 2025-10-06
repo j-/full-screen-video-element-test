e10f1e3cfcdee929e54469fba37af0e19320cf51
j- <j@skeoh.com>
2025-10-06 11:13:22 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# full-screen-video-element-test
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/j-/full-screen-video-element-test)
---
+++ src/main.ts
@@ -0,0 +1,63 @@
+const width = 100;
+const height = 100;
+
+const main = async () => {
+  if (!document.fullscreenEnabled) {
+    throw new Error('`document.fullscreenEnabled` was `' + document.fullscreenEnabled +  '`, expected `true`.');
+  }
+  
+  const video = document.getElementById('video') as HTMLVideoElement;
+  if (!video) throw new Error('No video element found');
+  
+  if (typeof video.requestFullscreen !== 'function') {
+    throw new Error('`typeof video.requestFullscreen` was `' + JSON.stringify(typeof video.requestFullscreen) +  '`, expected `"function""`.');
+  }
+  
+  const buttonFullscreen = document.getElementById('button-fullscreen') as HTMLButtonElement;
+  if (!buttonFullscreen) throw new Error('No fullscreen button found');
+
+  buttonFullscreen.addEventListener('click', async (e) => {
+    e.preventDefault();
+    try {
+      await video.requestFullscreen();
+    } catch (err) {
+      alert(`Error requesting full screen:\n\n${err}`);
+    }
+  });
+
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  if (!canvas) throw new Error('No canvas element found');
+
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  if (!ctx) throw new Error('Unable to get 2D rendering context');
+
+  const buttonColors = document.getElementsByClassName('button-color') as HTMLCollectionOf<HTMLButtonElement>;
+  if (buttonColors.length < 1) throw new Error('No color buttons found');
+
+  for (const buttonColor of buttonColors) {
+    buttonColor.addEventListener('click', async (e) => {
+      e.preventDefault();
+      const currentColor = (e.currentTarget as HTMLButtonElement).value;
+      try {
+        ctx.fillStyle = currentColor;
+        ctx.fillRect(0, 0, width, height);
+    
+        const stream = canvas.captureStream(0);
+        const [track] = stream.getVideoTracks() as CanvasCaptureMediaStreamTrack[];
+        track.requestFrame();
+    
+        video.srcObject = new MediaStream([track]);
+      } catch (err) {
+        alert(`Error updating test video:\n\n${err}`);
+      }
+    });
+  }
+};
+
+(async () => {
+  try {
+    await main();
+  } catch (err) {
+    alert(`Error initializing application:\n\n${err}`);
+  }
+})();
